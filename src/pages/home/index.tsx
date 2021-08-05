030637810480f9ec68e9ebf788a3c49f1319298b
Liam <liam@192.168.0.2>
2021-08-05 15:28:34 +0800

    fix: eg

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./home.scss";
 
-const Home = () => {
-  return (
-    <div>
-      home
-      <div className="test-css3"></div>
-    </div>
-  );
-};
+class Home extends React.Component {
+  state = {
+    number: 0,
+  };
+  handleClick = () => {
+    this.setState({ number: this.state.number + 1 });
+    console.log(this.state.number);
+    this.setState({ number: this.state.number + 1 });
+    console.log(this.state.number);
+  };
+  render() {
+    return <div onClick={this.handleClick}>点击 {this.state.number}</div>;
+  }
+}
 
 export default Home;
